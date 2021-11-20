c6d9548cde5de66644cc9259bb21a1ac895ff02a
DmitrySedyi <94315658+DmitrySedyi@users.noreply.github.com>
2021-11-20 08:28:43 +0200

    Update 4.py

--- HT_1/4.py
+++ HT_1/4.py
@@ -3,4 +3,4 @@ N = int(input("How many stings would you like to concatenate ?: "))
 for i in range(N):
         st = input("Input sting: ")
         ST += st
-print (ST)
+print ("Here we go: " + ST)
