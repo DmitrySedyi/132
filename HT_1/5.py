206c355547f80a544bd5d84d1635c6bfb730d1d0
DmitrySedyi <94315658+DmitrySedyi@users.noreply.github.com>
2021-11-20 13:01:01 +0200

    Update 5.py

--- HT_1/5.py
+++ HT_1/5.py
@@ -1,2 +1,21 @@
-decimal = int(input("Enter a number: "))
-print("Hexadecimal: ",hex(decimal))
+decimal = int(input("Enter the decimal value: "))
+
+porovny = decimal // 16
+ostatok = decimal % 16
+
+if ostatok == 10:
+    print ("hexadecimal: " + str(porovny) + "A")
+elif ostatok == 11:
+    print ("hexadecimal: " + str(porovny) + "B")
+elif ostatok == 12:
+    print ("hexadecimal: " + str(porovny) + "C")
+elif ostatok == 13:
+    print ("hexadecimal: " + str(porovny) + "D")
+elif ostatok == 14:
+    print ("hexadecimal: " + str(porovny) + "E")
+elif ostatok == 15:
+    print ("hexadecimal: " + str(porovny) + "F") 
+elif ostatok == 0:
+    print ("hexadecimal: " + str(porovny)) 
+else:
+    print ("hexadecimal: " + str(porovny) + str(ostatok))
