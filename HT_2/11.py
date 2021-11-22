8fae9ad211ed3e0ba141b4da30690f2dd9399afc
DmitrySedyi <94315658+DmitrySedyi@users.noreply.github.com>
2021-11-22 14:31:23 +0200

    Create 11.py

---
+++ HT_2/11.py
@@ -0,0 +1,11 @@
+#11. Write a script to remove duplicates from Dictionary.
+
+d = {1: 10, 2: 20, 3: 30, 4: 30, 5: 50, 6: 60}
+d2 = {}
+
+for uv in d.values():
+  for key, val in d.items():
+    if uv == val:s
+      d2[key] = val
+      breaks
+print (d2)
