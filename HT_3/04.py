e43a135906465df61a80ed38606d33fb0ae16cfa
DmitrySedyi <94315658+DmitrySedyi@users.noreply.github.com>
2021-11-25 12:22:44 +0200

    Update 04.py

--- HT_3/04.py
+++ HT_3/04.py
@@ -2,3 +2,27 @@
 #Також створiть четверу ф-цiю, яка в тiлi викликає 3 попереднi, обробляє повернутий ними результат та також повертає результат. 
 #Таким чином ми будемо викликати 1 функцiю, а вона в своєму тiлi ще 3
 
+a = int(input("Enter 1 number :"))
+b = int(input("Enter 2 number :"))
+c = int(input("Enter 3 number :"))
+
+
+def sum(a,b,c):
+  return a+b+c
+  
+def ymnoj(a,b,c): 
+  return a*b*c
+  
+def minus(a,b,c):
+  return a-b-c
+
+  
+def story():
+  print ("Сумма: ")
+  print(sum(a,b,c))
+  print ("Произведение: ")
+  print(ymnoj(a,b,c))
+  print ("Частное: ")
+  print(minus(a,b,c))
+
+print (story())
