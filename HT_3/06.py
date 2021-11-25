f9ca1ebe5fa1083352ba2672c9b85db04ee357d9
DmitrySedyi <94315658+DmitrySedyi@users.noreply.github.com>
2021-11-25 14:35:05 +0200

    Update 06.py

--- HT_3/06.py
+++ HT_3/06.py
@@ -3,3 +3,27 @@
 # -  якщо довжина рядка в діапазонi 30-50 -> прiнтує довжину, кiлькiсть букв та цифр
 # -  якщо довжина менше 30 -> прiнтує суму всiх чисел та окремо рядок без цифр (лише з буквами)
 # -  якщо довжина бiльше 50 - > ваша фантазiя
+
+s = input("Enter string: ")
+
+def result(s):
+  if len(s)	> 29 and len(s)	< 51:
+    print ("Длинна строки: ")
+    print (len(s))
+    print ("Количество букв: ")
+    print(len([i for i in s if i.isalpha()]))
+    print ("Количкство цифр: ")
+    print(len([i for i in s if i.isdigit()]))
+  elif len(s)	< 30:
+    print ("Cумма всех чисел: ")
+    k=0
+    for i in s:
+        if i>="0" and i<="9":
+          k += int(i)
+    print(k)
+    print ("Строка без цифр (только с буквами): ")
+  elif len(s)	> 50:
+    print("ваша фантазия")
+    
+    
+print(result(s))  
